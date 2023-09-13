5d01b5260e1c0aea10a634c2b1f035567cbf15ec
seif <seifeddine.aaza@gmail.com>
2023-09-14 00:16:13 +0100

    changes

--- main.js
+++ main.js
@@ -6,48 +6,56 @@ const products = [
         img: "nft1.jpg",
         name: "Bored Ape",
         price: 27.89,
+        quantity: 1
     },
     {
         id: 2,
         img: "nft2.jpg",
         name: "Bored Ace",
         price: 17.89,
+        quantity: 1
     },
     {
         id: 3,
         img: "nft3.jpg",
         name: "Bored Ace",
         price: 10.90,
+        quantity: 1
     },
     {
         id: 4,
         img: "nft4.jpg",
         name: "Bored ika",
         price: 13.80,
+        quantity: 1
     },
     {
         id: 5,
         img: "nft5.jpg",
         name: "Bored Kib",
         price: 22.80,
+        quantity: 1
     },
     {
         id: 6,
         img: "nft6.jpg",
         name: "Bored Bam",
         price: 19.40,
+        quantity: 1
     },
     {
         id: 7,
         img: "nft7.jpg",
         name: "Bored Lak",
         price: 18.35,
+        quantity: 1
     },
     {
         id: 8,
         img: "nft8.jpg",
         name: "Bored ika",
         price: 18.20,
+        quantity: 1
     },
     
 ]
@@ -89,14 +97,15 @@ const removeLikedItem = (itemId) => {
     liked = liked.filter(item => item.id != itemId);
     displayToCart(liked, "liked", likedBody)
 }
-const removeAddedItem = (itemId) => { 
+const removeAddedItem = (itemId, itemPrice) => { 
     added = added.filter(item => item.id != itemId)
     displayToCart(added, "added", addedBody)
+    total = total - itemPrice
+    insertTotal(total)
 }
 
 const push = (array, arrayName, obj) => {
     const id = obj.parentElement.parentElement.id
-
     products.map((product) => id == product.id ? item = product : null)
     let img = item.img, price = item.price, name = item.name
 
@@ -112,7 +121,7 @@ const likeToggle = (obj) => {
 }
 const add = (obj) => {
     push(added, "added", obj)
-    displayToCart(added, "added", addedBody)
+    displayToCart(added, "added", addedBody, obj)
 }
 const like = (obj) =>{
     likeToggle(obj)
@@ -140,16 +149,17 @@ const insertTotal = (total) => {
 totalContent.innerHTML = `Total : ${total.toFixed(2)} ETH`
 }
 
-const displayToCart = (array, arrayName, location) => array.length == 0 
-? location.innerHTML = `You have not ${arrayName} any item yet` 
-: location.innerHTML = array.map(item => `<div class="cart-product">
-<img src="./assets/images/${item.img}" />
-<div><h3>${item.price} ETH</h3><p>${item.name}</p></div>
-${arrayName == "liked" ? `<i onclick="removeLikedItem(${item.id})" class="bi bi-x-lg"></i>` 
-: `<i onclick="removeAddedItem(${item.id})" class="bi bi-trash3-fill"></i>`}
-</div><hr/>` ) 
+const displayToCart = (array, arrayName, location, obj) => {
+    array.length == 0 ? location.innerHTML = `You have not ${arrayName} any item yet` 
+    : location.innerHTML = array.map(item => `<div class="cart-product">
+    <img src="./assets/images/${item.img}" />
+    <div><h3>${item.price} ETH</h3><p>${item.name}</p></div>
+    ${arrayName == "liked" ? `<i onclick="removeLikedItem(${item.id})" class="bi bi-x-lg"></i>` 
+    : `<i onclick="removeAddedItem(${item.id}, ${item.price})" class="bi bi-trash3-fill"></i>`}
+    </div><hr/>` ) 
+}
 
-
+console.log(added);
 
 
 
